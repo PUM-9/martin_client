c8d4aaf646e3980d5a9bc9b74156396d9ffbd9f9
Martin <marlu819@student.liu.se>
2017-02-16 16:15:10 +0100

    Cleaned up code.

--- scripts/client.py
+++ scripts/client.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 
+import sys
 import rospy
 from martin_client.msg import Message
 
 
 def callback(data):
-    print(data.sender)
-    print(data.message)
+    if data.message and data.sender != "martin":
+        print(data.sender + ": " + data.message)
 
-if __name__ == "__main__":
-    rospy.init_node('martin_client', anonymous=False)
-    pub = rospy.Publisher('chat_in', Message, queue_size=10)
+if __name__ == "__main__":    
+    rospy.init_node('martin_client', anonymous=False)        
+    
+    pub = rospy.Publisher('chat_in', Message, queue_size=1000)
     sub = rospy.Subscriber('chat_out', Message, callback)
+    
     send_msg = Message()
     send_msg.sender = "martin"
-
+    
     while not rospy.is_shutdown():
-        input = str(input("Enter msg: "))
+        send_msg.message = raw_input()
         pub.publish(send_msg)
